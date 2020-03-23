09e11a3497f3cc1cad9c72a35e9c2ce4498c510a
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-03-23 15:37:20 -0300

    development of intro html and css

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,8 +2,14 @@ import React from "react"
 
 export default function Intro() {
 	return (
-		<div className="container">
-			<h1>Hello Intro</h1>
+		<div className="intro">
+			<h1>Descubra Como Ter Cabelos Longos e Volumosos em 30 Segundos</h1>
+			<div>
+				<p>Sem Grampos, Sem Cola, Sem Danos❤️ </p>
+				<div>
+					<a href="/">Escolher Aplique!</a>
+				</div>
+			</div>
 		</div>
 	)
 }
