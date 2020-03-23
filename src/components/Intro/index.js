f377ca5e893c554e9130f77207bf9959091c4ce6
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-03-23 18:22:15 -0300

    html and css development

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,15 +1,50 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+import "./styles.css"
 
-export default function Intro() {
+const BackgroundSection = ({ className }) => (
+	<StaticQuery
+		query={graphql`
+			query {
+				desktop: file(relativePath: { eq: "intro.webp" }) {
+					childImageSharp {
+						fluid(quality: 90, maxWidth: 1920) {
+							...GatsbyImageSharpFluid_withWebp
+						}
+					}
+				}
+			}
+		`}
+		render={data => {
+			// Set ImageData.
+			const imageData = data.desktop.childImageSharp.fluid
+			return (
+				<BackgroundImage
+					Tag="section"
+					className={`intro-background`}
+					fluid={imageData}
+				>
+					<div className="intro-content">
+						<h2 className="intro-title">
+							Descubra Como Ter Cabelos Longos e Volumosos em 30 Segundos
+						</h2>
+						<p>Sem Grampos, Sem Cola, Sem Danos ❤️</p>
+						<a href="/">
+							{" "}
+							<button>Escolher Aplique! </button>{" "}
+						</a>
+					</div>
+				</BackgroundImage>
+			)
+		}}
+	/>
+)
+
+export default function Intro({ className }) {
 	return (
 		<div className="intro">
-			<h1>Descubra Como Ter Cabelos Longos e Volumosos em 30 Segundos</h1>
-			<div>
-				<p>Sem Grampos, Sem Cola, Sem Danos❤️ </p>
-				<div>
-					<a href="/">Escolher Aplique!</a>
-				</div>
-			</div>
+			<BackgroundSection />
 		</div>
 	)
 }
