f75852c526ed91de2f6b1526138c87b97801ef4b
viniciusmdias <vinicius-mdias@hotmail.com>
2020-03-26 15:36:13 -0300

    Create a recomendations component

---
+++ src/components/Recomendations/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import "./styles.css"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+export default function Recomendations() {
+	const data = useStaticQuery(graphql`
+		query ImgRecomendations {
+			image: file(relativePath: { eq: "recomendations.png" }) {
+				childImageSharp {
+					fluid {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`)
+	return (
+		<div className="container recomendations">
+			<h3>Aplique Internacionalmente Recomendado Por:</h3>
+			<Img
+				fluid={data.image.childImageSharp.fluid}
+				alt="Imagem com logo de marcas que recomendam a Bela Belinda"
+			/>
+		</div>
+	)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Intro from "../components/Intro"
+import Recomendations from "../components/Recomendations"
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Bela Belinda" />
 		<Intro />
+		<Recomendations />
 	</Layout>
 )
 
