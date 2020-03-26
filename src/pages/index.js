0aa4c961c4b1d0c1e7936973610fd64c02468525
viniciusmdias <vinicius-mdias@hotmail.com>
2020-03-26 17:08:30 -0300

    Create a "Transformation" component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Intro from "../components/Intro"
 import Recomendations from "../components/Recomendations"
+import Transformation from "../components/Transformation"
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Bela Belinda" />
 		<Intro />
 		<Recomendations />
+		<Transformation />
 	</Layout>
 )
 
