3eed408bbc01448db97a93d0c5383bad86305db8
viniciusmdias <vinicius-mdias@hotmail.com>
2020-03-26 16:44:27 -0300

    Create a component "WhatIs"

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Intro from "../components/Intro"
 import Recomendations from "../components/Recomendations"
+import WhatIs from "../components/WhatIs"
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Bela Belinda" />
 		<Intro />
 		<Recomendations />
+		<WhatIs />
 	</Layout>
 )
 
