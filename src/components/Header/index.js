f5de26e13d48df19322e056f16c92a65e014619c
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-04-01 16:30:53 -0300

    Fix html syntax problems
    Altered class to className
    Altered <option value="BRL" selected="selected"> to <option defaultValue="BRL">

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -84,11 +84,11 @@ export default function Header() {
 						<div className="login-desktop">
 							<a href="https://www.belabelinda.com/account/login">Entrar</a>
 							<select
-								class="currency-picker"
+								className="currency-picker"
 								name="currencies"
 								data-default-shop-currency="BRL"
 							>
-								<option value="BRL" selected="selected">
+								<option defaultValue="BRL" >
 									BRL
 								</option>
 								<option value="INR">INR</option>
@@ -101,12 +101,12 @@ export default function Header() {
 							</select>
 						</div>
 						<div className="carrinho">
-							<a href="https://belabelinda.com/cart" class="cart">
+							<a href="https://belabelinda.com/cart" className="cart">
 								<svg
 									aria-hidden="true"
 									focusable="false"
 									role="presentation"
-									class="icon icon-cart"
+									className="icon icon-cart"
 									viewBox="0 0 37 40"
 								>
 									<path d="M36.5 34.8L33.3 8h-5.9C26.7 3.9 23 .8 18.5.8S10.3 3.9 9.6 8H3.7L.5 34.8c-.2 1.5.4 2.4.9 3 .5.5 1.4 1.2 3.1 1.2h28c1.3 0 2.4-.4 3.1-1.3.7-.7 1-1.8.9-2.9zm-18-30c2.2 0 4.1 1.4 4.7 3.2h-9.5c.7-1.9 2.6-3.2 4.8-3.2zM4.5 35l2.8-23h2.2v3c0 1.1.9 2 2 2s2-.9 2-2v-3h10v3c0 1.1.9 2 2 2s2-.9 2-2v-3h2.2l2.8 23h-28z"></path>
@@ -127,11 +127,11 @@ export default function Header() {
 						<div className="login">
 							<a href="https://www.belabelinda.com/account/login">Entrar</a>
 							<select
-								class="currency-picker"
+								className="currency-picker"
 								name="currencies"
 								data-default-shop-currency="BRL"
 							>
-								<option value="BRL" selected="selected">
+								<option defaultValue="BRL" >
 									BRL
 								</option>
 								<option value="INR">INR</option>
@@ -146,7 +146,7 @@ export default function Header() {
 						</div>
 					</div>
 				</div>
-				<div class="frete">
+				<div className="frete">
 					<div className="social-icons">
 						<a href="https://www.facebook.com/belabelindaapliques/">
 							<FaFacebookSquare />
@@ -156,7 +156,7 @@ export default function Header() {
 						</a>
 					</div>
 					<p> A belabelinda paga o seu frete ❤️</p>
-					<p class="email">
+					<p className="email">
 						<a href="mailto:suporte.belabelinda@gmail.com">
 							suporte.belabelinda@gmail.com
 						</a>
