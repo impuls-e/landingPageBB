f338e719b49682fab007c4265df741b8b0789b90
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-01 13:14:25 -0300

    Create a component for video

--- src/components/HowToUse/index.js
+++ src/components/HowToUse/index.js
@@ -1,22 +1,36 @@
-import React from "react"
+import React from 'react'
 
-import "./styles.css"
-
+import { useStaticQuery, graphql } from 'gatsby'
+import './styles.css'
+import Video from '../Video'
 export default function HowToUse() {
+	const data = useStaticQuery(graphql`
+		query VideoInicialQuery {
+			allMdx(filter: { frontmatter: { tag: { eq: "video-inicial" } } }) {
+				edges {
+					node {
+						frontmatter {
+							videoSrcURL
+							videoTitle
+						}
+					}
+				}
+			}
+		}
+	`)
+
 	return (
 		<div className="container how-to-use">
 			<div className="htu-wrap">
 				<h3>Veja Como é Super Simples De Usar</h3>
 				<h4>Seu Melhor Look Sem Depender de Ninguém!</h4>
 				<div className="iframe-container">
-					<iframe
+					<Video
+						videoSrcURL={data.allMdx.edges[0].node.frontmatter.videoSrcURL}
+						videoTitle={data.allMdx.edges[0].node.frontmatter.videoTitle}
 						width="1140"
 						height="641"
-						src="https://www.youtube.com/embed/zXxXoqp9PBY"
-						frameBorder="0"
-						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-						allowfullscreen="allowfullscreen"
-					></iframe>
+					/>
 				</div>
 			</div>
 		</div>
--- src/components/RealClientsSlick/index.js
+++ src/components/RealClientsSlick/index.js
@@ -1,8 +1,8 @@
-import React from "react"
-import "./styles.css"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import './styles.css'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Img from "gatsby-image"
+import Img from 'gatsby-image'
 
 export default function RealClientsSlick() {
 	const data = useStaticQuery(graphql`
