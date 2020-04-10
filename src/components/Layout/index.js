815718b83ce4f0e3a1582c9c35637ad60f6c5559
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-10 14:57:36 -0300

    Add script to form

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -29,6 +29,11 @@ const Layout = ({ children }) => {
 			<div>
 				<main>{children}</main>
 			</div>
+			<script
+				async
+				type="text/javascript"
+				src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=L4udT4"
+			></script>
 		</>
 	)
 }
